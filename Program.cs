7656ffd5a3c3d0daba2225d89ce6bb9e572bebe0
mhmdfakhrim <muhfakhri4@gmail.com>
2022-12-26 17:14:38 +0700

    latihan

--- Program.cs
+++ Program.cs
@@ -13,6 +13,18 @@ namespace BinarySearchTree
          */
 
         class node
+        {
+            public string info;
+            public node lchild;
+            public node rchild;
+
+            //constructor for the node class
+
+            public node(string i, node l, node r)
+            {
+                info = i;
+            }
+        }
         static void Main(string[] args)
         {
         }
