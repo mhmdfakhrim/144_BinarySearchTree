fc0ba8b65fa44085da042419f3ca04dbe0a4ed2a
mhmdfakhrim <muhfakhri4@gmail.com>
2022-12-26 17:49:58 +0700

    latihan

--- Program.cs
+++ Program.cs
@@ -6,67 +6,109 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    class Program
+
+    /*A node class consi
+     * 
+     */
+
+    class node
     {
-        /*A node class consi
-         * 
-         */
+        public string info;
+        public node lchild;
+        public node rchild;
 
-        class node
+        //constructor for the node class
+
+        public node(string i, node l, node r)
         {
-            public string info;
-            public node lchild;
-            public node rchild;
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
 
-            //constructor for the node class
 
-            public node(string i, node l, node r)
-            {
-                info = i;
-                lchild = l;
-                rchild = r;
-            }
+
+    class BinaryTree
+    {
+        public node ROOT;
+
+        public BinaryTree()
+        {
+            ROOT = null; // initializing root to null
         }
 
-        class BinaryTree
+        public void insert(string element)//insert a node in the binary
         {
-            public node ROOT;
-
-            public BinaryTree()
+            node tmp, parent = null, currentnode = null;
+            find(element, ref parent, ref currentnode);
+            if (currentnode != null) //
             {
-                ROOT = null; // initializing root to null
+                Console.WriteLine("Duplicate words not allowed");
+                return;
             }
-
-            public void insert(string element)//insert a node in the binary
+            else //if the specified node is not present
             {
-                node tmp, parent = null, currentnode = null;
-                find(element, ref parent, ref currentnode);
-                if (currentnode != null) //
+                tmp = new node(element, null, null);
+                if (parent == null)
                 {
-                    Console.WriteLine("Duplicate words not allowed");
-                    return;
+                    ROOT = tmp;
                 }
-                else //if the specified node is not present
+                else if (string.Compare(element, parent.info) < 0)
                 {
-                    tmp = new node(element, null, null);
-                    if(parent == null)
-                    {
-                        ROOT = tmp;
-                    }
-                    else if (string.Compare(element, parent.info) <0)
-                    {
-                        if (string.Compare(element, parent.info) <0)
-                            parent.lchild = tmp;
-                    }
-                    else
-                    {
-                        parent.rchild = tmp;
-                    }
+                    if (string.Compare(element, parent.info) < 0)
+                        parent.lchild = tmp;
+                }
+                else
+                {
+                    parent.rchild = tmp;
                 }
             }
         }
-        static void Main(string[] args)
+
+        public void fin(string element, ref node parent, ref node currentnode)
         {
+
+            currentnode = ROOT;
+            parent = null;
+            while ((currentnode != null) && (currentnode.info != element))
+            {
+                parent = currentnode;
+                if (string.Compare(element, currentnode.info) < 0)
+                    currentnode = currentnode.lchild;
+                else
+                    currentnode = currentnode.rchild;
+            }
         }
+
+        public void inorder(node ptr)
+        {
+            Console.WriteLine("Tree is empty");
+            return;
+        }
+            if(ptr != null)
+                {
+                    inorder(ptr.lchild);
+                    Console.WriteLine(IntPtr.info +"");
+                    inorder(ptr.rchild);
+                }
+        }
+
+    public void preorder(node ptr) 
+    { 
+        if(ROOT == null)
+    {
+        Console.WriteLine("Tree is empty");
+        return ;
+    }
+        if (ptr != null)
+    {
+        Console.WriteLine(ptr.info + "");
+        preorder(ptr.lchild);
+        preorder(ptr.rchild);
+    }
+    }
+    public static void Main(string[] args)
+    {
     }
 }
