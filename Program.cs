5a5bfe3679e6fcd574a6b52f1f38616cf43d69f8
mhmdfakhrim <muhfakhri4@gmail.com>
2022-12-26 17:27:17 +0700

    latihan

--- Program.cs
+++ Program.cs
@@ -38,6 +38,32 @@ namespace BinarySearchTree
             }
 
             public void insert(string element)//insert a node in the binary
+            {
+                node tmp, parent = null, currentnode = null;
+                find(element, ref parent, ref currentnode);
+                if (currentnode != null) //
+                {
+                    Console.WriteLine("Duplicate words not allowed");
+                    return;
+                }
+                else //if the specified node is not present
+                {
+                    tmp = new node(element, null, null);
+                    if(parent == null)
+                    {
+                        ROOT = tmp;
+                    }
+                    else if (string.Compare(element, parent.info) <0)
+                    {
+                        if (string.Compare(element, parent.info) <0)
+                            parent.lchild = tmp;
+                    }
+                    else
+                    {
+                        parent.rchild = tmp;
+                    }
+                }
+            }
         }
         static void Main(string[] args)
         {
