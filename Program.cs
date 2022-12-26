557ffcca360a55125dd88d44b51c6c36f98cbb78
mhmdfakhrim <muhfakhri4@gmail.com>
2022-12-26 17:13:09 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        /*A node class consi
+         * 
+         */
+
+        class node
+        static void Main(string[] args)
+        {
+        }
+    }
+}
